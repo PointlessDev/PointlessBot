e5a1365254759c7345f1c51bc0f4bedec9e964a8
Pointless <me@pointless.me>
2017-08-17 13:44:48 +1200

    nonnonononononononononononononononnonononononononnonononononononononono
    I should have finished that

--- src/commands/invite-remover.ts
+++ src/commands/invite-remover.ts
@@ -6,7 +6,7 @@ import {Message, Client} from 'discord.js';
 export default class InviteRemovalCommand {
   constructor(private client: Client) {
     client.on('message', message => {
-      if(message.content.toLowerCase()) {
+      if(message.content.toLowerCase() && false) {
         message.delete();
         message.reply('Oi you, no posting invites.');
       }
