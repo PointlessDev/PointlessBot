83746d0e332038d25ef883616bad06c60515c534
Pointless <me@pointless.me>
2017-10-12 15:50:07 +1300

    Updated eval to be ∞% less secure

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -4,6 +4,9 @@
 import {Command, Arguments, Authorization} from 'discordthingy';
 import {Message} from 'discord.js';
 import {VM} from 'vm2';
+import * as vm from 'vm';
+
+const secure = false;
 
 export default class EvalCommand {
   @Command({
@@ -12,17 +15,14 @@ export default class EvalCommand {
   })
   public eval(message: Message, args: Arguments) {
     const code = args.contentFrom(1);
-    const vm = new VM({
-      sandbox: {
-        message
-      },
-      timeout: 1000
-    });
+    const script = new vm.Script(code);
     const start = process.hrtime();
     let desc;
     let success = false;
     try {
-      let returned = vm.run(code);
+      let returned = script.runInNewContext({
+        message: message
+      });
       success = true;
       if(returned instanceof Object) desc = JSON.stringify(returned, null, 2);
       else if(typeof returned === 'string') desc = `"${returned}"`;
